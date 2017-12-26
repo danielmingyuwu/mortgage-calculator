073295019cbc5379387ac08796f00bb44a109ed1
Daniel <daniel.my.wu@gmail.com>
2017-12-26 01:09:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert('Hello World!');
+function calculate(){
+ var loan = parseInt(document.getElementById("loan").value);
+ var rate = parseFloat(document.getElementById("rate").value);
+ var years = parseInt(document.getElementById("years").value);
+
+ // todo: actually implement the mortgage 
+ var result = loan * rate * years;
+ alert(result);
+}
